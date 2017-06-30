156fdd031becbe648869b8543d3b7f5d462790d5
Todd M. Edgar <tmedgar@users.noreply.github.com>
2017-06-30 16:44:23 -0400

    Added Get_Random_DYK function
    Pulls a DYK from pool of all parks (not just from pool of one park).

--- lambda_function.py
+++ lambda_function.py
@@ -35,6 +35,8 @@ def on_intent(intent_request, session):
         return get_park_news(intent)
     elif intent_name == "GetParkDYK":
         return get_park_dyk(intent)
+    elif intent_name == "GetRandomDYK":
+        return get_random_dyk(intent)
     elif intent_name == "GetParksByState":
         return get_parks_by_state(intent)
     elif intent_name == "AMAZON.HelpIntent":
@@ -167,6 +169,20 @@ def get_park_dyk(intent):
             reprompt_text = ""
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+
+def get_random_dyk(intent):
+    session_attributes = {}
+    should_end_session = False
+    card_title = "National Park Trivia"
+    # construct request and parse results
+    request_url = TRIVIA_BASE
+    req = urllib.request.Request(request_url)
+    response = urllib.request.urlopen(req).read()
+    data = json.loads(response.decode('utf-8'))
+    speech_output = data[0]['parkName'] + " Trivia: " + data[0]['fact']
+    reprompt_text = ""
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session)) 
 
 def get_parks_by_state(intent):
     session_attributes = {}
